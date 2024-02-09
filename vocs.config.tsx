536a32e2bb066de00655639eae818a09c38ce76c
Seranged <seranged@gmail.com>
2024-02-09 13:27:17 +0000

    Chore: update all metadata associated stuff so in alignment with others, update black/white logos

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -4,7 +4,7 @@ export default defineConfig({
   head: (
     <>
       <script
-      /* biome-ignore lint/security/noDangerouslySetInnerHtml: <TS Doesn't like the offical vercel example> */
+        /* biome-ignore lint/security/noDangerouslySetInnerHtml: <TS Doesn't like the offical vercel example> */
         dangerouslySetInnerHTML={{
           __html: `
           window.va = window.va || function (...args) { 
@@ -14,17 +14,49 @@ export default defineConfig({
         }}
       />
       <script defer src="/_vercel/insights/script.js" />
+      <title>Gas.zip · Technical Documentation for Onchain Interactions</title>
+      <meta name="title" content="Gas.zip · Technical Documentation for Onchain Interactions" />
+      <meta
+        name="description"
+        content={
+          'Comprehensive guides and documentation to help you start working with Gas.zip on chain as quickly as possible. Learn how to interact with Gas.Zip contracts on-chain, and get detailed information about supported chains and code examples.'
+        }
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="robots" content="index, follow" />
       <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.gas.zip/" />
       <meta
         property="og:title"
         content="Gas.zip · Technical Documentation for Onchain Interactions"
       />
-      <meta property="og:image" content="/gasMeta.png" />
-      <meta property="og:url" content="https://gas.zip" />
       <meta
         property="og:description"
-        content="Comprehensive guides and documentation to help you start working with Gas.zip on chain as quickly as possible. Learn how to interact with Gas.Zip contracts on-chain, and get detailed information about supported chains and code examples."
+        content={
+          'Fastest one-stop gas refuel bridge for all LayerZero powered chains. Bridge and explore all your favorite alt-chains and L2s.'
+        }
       />
+      <meta property="og:image" content="http://gas.zip/gasMeta.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="mask-icon" href="/faviconsafari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:url" content="https://www.lz.gas.zip/" />
+      <meta
+        property="twitter:title"
+        content="Gas.zip · Technical Documentation for Onchain Interactions"
+      />
+      <meta
+        property="twitter:description"
+        content={
+          'Fastest one-stop gas refuel bridge for all LayerZero powered chains. Bridge and explore all your favorite alt-chains and L2s.'
+        }
+      />
+      <meta property="twitter:image" content="https://i.imgur.com/5w0YMW3.png" />
     </>
   ),
   font: {

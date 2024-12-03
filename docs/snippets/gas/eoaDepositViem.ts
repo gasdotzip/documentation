8ae8bd26f41962ae79ef8332cac5c42696781b39
Seranged <seranged@gmail.com>
2024-12-03 11:59:36 +0000

    update eoaDepositViem to have additional address checks

--- docs/snippets/gas/eoaDepositViem.ts
+++ docs/snippets/gas/eoaDepositViem.ts
@@ -3,6 +3,7 @@ import { privateKeyToAccount } from 'viem/accounts'
 import { optimism } from 'viem/chains'
 import { depositABI } from './depositABI'
 import bs58 from 'bs58'
+import { PublicKey } from '@solana/web3.js'
 
 // Create a wallet from a private key
 const account = privateKeyToAccount('0x...')
@@ -26,22 +27,40 @@ const gasZipShortChainIDs = [51, 52]
 const encodeTransactionInput = (to: string, shorts: number[]) => {
   let data = '0x'
   if (to.toLowerCase() !== account.address?.toLowerCase()) {
-    if (to.length === 42) { // evm address
+    if (to.length === 42) {
       data += '02'
       data += to.slice(2)
-    } else if (new RegExp(/[1-9A-HJ-NP-Za-km-z]{32,44}/).test(to)) { // base58 sol address
-      data += '03'
-      data += Buffer.from(bs58.decode(to)).toString('hex')
+    } else if (to.length === 66) {
+      data += '04'
+      data += to.slice(2)
+    } else if (new RegExp(/[1-9A-HJ-NP-Za-km-z]{32,44}/).test(to)) {
+      const decoded = bs58.decode(to)
+      if (decoded.length === 32 && PublicKey.isOnCurve(to)) {
+        // sol
+        data += '03'
+        data += Buffer.from(decoded).toString('hex')
+      } else {
+        // other base58
+        const hexaddr = Buffer.from(decoded).toString('hex')
+        if (hexaddr.length === 50) {
+          data += '02'
+          data += hexaddr.slice(2).slice(0, hexaddr.length - 10)
+        } else {
+          data += '03'
+          data += hexaddr.slice(0, hexaddr.length - 8)
+        }
+      }
     } else {
       return null
     }
   } else {
-    data += '01' // to == sender
+    data += '01'
   }
 
   for (const i in shorts) {
     data += toHex(shorts[i]).slice(2).padStart(4, '0')
   }
+
   return data
 }
 

5cf6fa85166177b71371bc6589b88e04ca54047c
tk <58927867+tomkysar@users.noreply.github.com>
2025-06-21 05:48:50 -0700

    vana chain

--- data/gas/inboundChains.ts
+++ data/gas/inboundChains.ts
@@ -522,6 +522,16 @@ export const inboundChains: InboundChain[] = [
     }
   },
   {
+    name: "Vana",
+    chainId: 1480,
+    directAddress: "0x391E7C679d29bD940d63be94AD22A25d25b5A604",
+    contractAddress: "0x9E22ebeC84c7e4C4bD6D4aE7FF6f4D436D6D8390",
+    explorer: {
+      url: "https://vanascan.io/",
+      name: "VanaScan"
+    }
+  },
+  {
     name: "Nibiru",
     chainId: 6900,
     directAddress: "0x391E7C679d29bD940d63be94AD22A25d25b5A604",
